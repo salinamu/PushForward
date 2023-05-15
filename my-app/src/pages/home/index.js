51218d2dadcfe8119f2fcc6e094239966647cebe
salinamu <salinamu@users.noreply.github.com>
2023-05-15 16:11:56 -0500

    add timeline

--- my-app/src/pages/home/index.js
+++ my-app/src/pages/home/index.js
@@ -1,19 +1,149 @@
 import styles from "../../styles/Home.module.css";
 import Navbar from "../../comps/Navbar";
 const Home = () => {
-    return (
-      <div>
-        <div className={styles.container}>
-        <Navbar/>
+  return (
+    <div className={styles.container}>
+      <div className={styles.subcontainer}>
+        <Navbar />
 
-          <div className = {styles.bg}></div>
-          <div className = {styles.heading}>
+        <div className={styles.bg}></div>
+        <div className={styles.heading}>
           <h1>Push Forward</h1>
           <h2>Overcoming stagnant progress in racial justice </h2>
+        </div>
+      </div>
+      <div className={styles.subcontainer}>
+        <div className={styles.flexrow}>
+          <div className={styles.s1}>
+            <div className={styles.headings1}>
+              <h3>50 Years</h3>
+              <h2 className={styles.rightalign}>Little Growth</h2>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+              viverra vitae. Suspendisse tincidunt tellus pretium in lectus.
+              Pretium ante pellentesque posuere sed ut neque nec. Sit mattis
+              molestie morbi diam suscipit.
+            </p>
+          </div>
+          <div className={styles.s2}>
+          <div className={styles.time}>
+              <div className={styles.timetext}>
+                <h4 className={styles.name}>Event 1</h4>
+                <p className={styles.detail}>
+                  Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+                  viverra vitae.
+                </p>
+              </div>
+              <div className={styles.timeline}>
+                <div className={styles.line} />
+                <div className={styles.box} />
+                <div className={styles.line} />
+              </div>
+            </div>
+            <div className={styles.time}>
+              <div className={styles.timetext}>
+                <h4 className={styles.name}>Event 1</h4>
+                <p className={styles.detail}>
+                  Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+                  viverra vitae.
+                </p>
+              </div>
+              <div className={styles.timeline}>
+                <div className={styles.line} />
+                <div className={styles.box} />
+                <div className={styles.line} />
+              </div>
+            </div>
+            <div className={styles.time}>
+              <div className={styles.timetext}>
+                <h4 className={styles.name}>Event 1</h4>
+                <p className={styles.detail}>
+                  Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+                  viverra vitae.
+                </p>
+              </div>
+              <div className={styles.timeline}>
+                <div className={styles.line} />
+                <div className={styles.box} />
+                <div className={styles.line} />
+              </div>
+            </div>
+            <div className={styles.time}>
+              <div className={styles.timetext}>
+                <h4 className={styles.name}>Event 1</h4>
+                <p className={styles.detail}>
+                  Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+                  viverra vitae.
+                </p>
+              </div>
+              <div className={styles.timeline}>
+                <div className={styles.line} />
+                <div className={styles.box} />
+                <div className={styles.line} />
+              </div>
+            </div>
+            <div className={styles.time}>
+              <div className={styles.timetext}>
+                <h4 className={styles.name}>Event 1</h4>
+                <p className={styles.detail}>
+                  Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+                  viverra vitae.
+                </p>
+              </div>
+              <div className={styles.timeline}>
+                <div className={styles.line} />
+                <div className={styles.box} />
+                <div className={styles.line} />
+              </div>
+            </div>
+            <div className={styles.time}>
+              <div className={styles.timetext}>
+                <h4 className={styles.name}>Event 1</h4>
+                <p className={styles.detail}>
+                  Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+                  viverra vitae.
+                </p>
+              </div>
+              <div className={styles.timeline}>
+                <div className={styles.line} />
+                <div className={styles.box} />
+                <div className={styles.line} />
+              </div>
+            </div>
+            <div className={styles.time}>
+              <div className={styles.timetext}>
+                <h4 className={styles.name}>Event 1</h4>
+                <p className={styles.detail}>
+                  Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+                  viverra vitae.
+                </p>
+              </div>
+              <div className={styles.timeline}>
+                <div className={styles.line} />
+                <div className={styles.box} />
+                <div className={styles.line} />
+              </div>
+            </div>
+            <div className={styles.time}>
+              <div className={styles.timetext}>
+                <h4 className={styles.name}>Event 1</h4>
+                <p className={styles.detail}>
+                  Lorem ipsum dolor sit amet consectetur. Dolor etiam faucibus
+                  viverra vitae.
+                </p>
+              </div>
+              <div className={styles.timeline}>
+                <div className={styles.line} />
+                <div className={styles.box} />
+                <div className={styles.line} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default Home;
+    </div>
+  );
+};
+
+export default Home;
